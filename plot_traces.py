e0bf33beb0429ea1532a6240748aec5ddd5f535b
Francesco Cavarretta <francescocavarretta@hotmail.it>
2022-12-10 13:27:24 -0500

    plot traces re-adapted to the new output format

--- plot_traces.py
+++ plot_traces.py
@@ -2,12 +2,15 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import numpy as np
     import sys
-    traces = np.load(sys.argv[-1], allow_pickle=True).tolist()
-
+    data = np.load(sys.argv[-1], allow_pickle=True).tolist()
+    traces = data['responses']
+    title = data['key']
+    
     ncol = int(np.sqrt(len(traces)))
     nrow = int(np.ceil(len(traces) / ncol))
 
     plt.figure(figsize=(15, 15))
+    plt.title(str(title))
     for i, (k, tr) in enumerate(traces.items()):
         plt.subplot(ncol, nrow, i+1)
         plt.title(k)
